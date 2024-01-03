6c183b7d5d899f645b8cbfafc13fbb43021e81c6
MihiroSaito <61680540+MihiroSaito@users.noreply.github.com>
2024-01-03 22:20:32 +0900

    リファクタリング

--- lib/src/models/search_tree.dart
+++ lib/src/models/search_tree.dart
@@ -3,22 +3,29 @@ import 'package:what_is/src/models/search_web_page.dart';
 /// 検索情報を親子兄弟で紐づけておくためのクラス
 /// ```
 /// SearchTree(
+///    id: 1
 ///    searchWebPage: ****,
 ///    children: [
 ///       SearchTree(
+///         id: 2
 ///         searchWebPage: ****,
 ///         children: [
-///           SearchTree(searchWebPage: ****),
+///           SearchTree(id: 5, searchWebPage: ****),
 ///         ]
 ///       ),
-///       SearchTree(searchWebPage: ****),
-///       SearchTree(searchWebPage: ****),
+///       SearchTree(id: 3, searchWebPage: ****),
+///       SearchTree(id: 4, searchWebPage: ****),
 ///    ]
 /// )
 /// ```
 ///
 ///
 class SearchTree {
+
+  /// TreeのId（内部的にはIndexedStackに表示する時のIndexが利用されている）
+  /// * 外部からは指定できない。
+  ///
+  final int id;
 
   /// この階層のWebページ情報
   ///
@@ -28,8 +35,15 @@ class SearchTree {
   ///
   final List<SearchTree>? children;
 
-  const SearchTree({
+  SearchTree({
     required this.searchWebPage,
     this.children
-  });
+  }) : id = generateTreeId(searchWebPage.indexedStackIndex);
+
+
+  // indexedStackIndexをそのままIDに利用するだけのロジックだが、別ロジックでこれを意識させないためのもの
+  static generateTreeId(int indexedStackIndex) {
+    return indexedStackIndex;
+  }
+
 }
